61af1b84f96c172cdb44e03de7446040a21c5516
Tim Staudinger <t.staudinger96@web.de>
2022-08-13 16:27:57 +0200

    Update create_project.sh
    1) Add file type which should be created
    2) Check if all parameters are given

--- src/create_project.sh
+++ src/create_project.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 function createProject() {
-  # create repository
-  cd
-  cd Desktop/Repository.nosync/AutomateCreatingProjects/src
-  python repository.py $1
+  if [ $# -eq 2 ]; then
 
-  # clone repository
-  cd
-  cd Desktop/Repository.nosync
-  git clone https://github.com/StaudTim/$1.git
-  cd $1
+    # create repository
+    cd
+    cd Desktop/Repository.nosync/AutomateCreatingProjects/src
+    python repository.py $1
 
-  # readme
-  touch README.md
-  open README.md
+    # clone repository
+    cd
+    cd Desktop/Repository.nosync
+    git clone https://github.com/StaudTim/$1.git
 
-  # src
-  mkdir src
-  cd src
-  touch $1.py
-  open $1.py
+    # readme
+    cd $1
+    touch README.md
+    open README.md
+
+    # src
+    mkdir src
+    cd src
+    touch main.$2
+    open main.$2
+
+  else
+    echo "supply arguments: 1) name of the project 2) file type (py, r)"
+
+  fi
 }
